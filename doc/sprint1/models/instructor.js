488842a2dfff86a5083f03d8d7b2220692f4eb3e
Julio Delgado <jd85451@gm>
2021-06-04 21:54:03 -0400

    MJAAR-5 Adding schema models of Entrepreneur, Instructor, Company and Partner

---
+++ doc/sprint1/models/instructor.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const InstructorSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    classes: [{
+        type: String,
+    }],
+    image: {
+        type: String                // Url of image
+    },
+    biography: String
+})
+
+module.exports = mongoose.model("Instructor", InstructorSchema);
