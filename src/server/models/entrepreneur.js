5ecadee70d0451eeee19732aa44a166fba241541
Julio Delgado <jd85451@gmail.com>
2021-06-08 00:42:00 -0400

    Updating models

--- src/server/models/entrepreneur.js
+++ src/server/models/entrepreneur.js
@@ -2,22 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const EntrepreneurSchema = new Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    }, 
-    company: {
-        type: String,
-        required: true
-    },
+    company: String,
     location: String,
     age: Number,
     biography: String,
--- src/server/models/instructor.js
+++ src/server/models/instructor.js
@@ -2,18 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const InstructorSchema = new Schema({
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    }, 
-    username: {
-        type: String,
-        required: true
-    }, 
     classes: [{
         type: String,
     }],
