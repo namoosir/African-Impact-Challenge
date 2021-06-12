2531f2736a8f5b594ec462a14b32084a6d9f15b2
namoosir <saqeebnazmus@gmail.com>
2021-06-11 23:50:35 -0400

    Fixing issues

--- src/client/src/components/login/login.js
+++ src/client/src/components/login/login.js
@@ -1,4 +1,4 @@
-import Header from "../Header";
+import Header from "../header";
 
 import { Component } from "react";
 
--- src/client/src/components/register/register.js
+++ src/client/src/components/register/register.js
@@ -1,4 +1,4 @@
-import Header from "../Header";
+import Header from "../header";
 import Selection from "./selection";
 import { Component } from "react";
 
