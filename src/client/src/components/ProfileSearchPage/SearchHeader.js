c11dea4faca68070aca04e4ffac943c6d77b14c9
Andy Yuen <andyren.yuen@mail.utoronto.ca>
2021-07-08 17:17:18 -0400

    search functionality based on name

--- src/client/src/components/ProfileSearchPage/SearchHeader.js
+++ src/client/src/components/ProfileSearchPage/SearchHeader.js
@@ -1,34 +1,38 @@
 import Multiselect from 'multiselect-react-dropdown';    //"https://www.npmjs.com/package/multiselect-react-dropdown"
 import { func } from 'prop-types';
-import { Component, useState, useEffect } from 'react'
+import { Component, useState, useEffect, createRef } from 'react'
 
 
 const SearchHeader = ({users, setUsers, users2, setUsers2}) => {
 
-    const [options, setOptions] = useState({
-        options: [{entity: 'Company', id: 1},{entity: 'Partner', id: 2},{entity: 'Entrepreneur', id: 3},{entity: 'Instructor', id: 4}]
+    const [multiSelRef, setMultiSelRef] = useState({
+        userRef: createRef(),
+        companyRef: createRef()
+        
     })
-    const [options_company, setOptions_company] = useState({
-        options: [
-                {status: 'Seeks Funds',
-                    id: 1,
-                    set: false
-                }
-                ]
-    })
+
+    const [options, setOptions] = useState([
+        {entity: 'Company', id: 1},
+        {entity: 'Partner', id: 2},
+        {entity: 'Entrepreneur', id: 3},
+        {entity: 'Instructor', id: 4}
+    ])
+    const [options_company, setOptions_company] = useState([
+        {status: 'Seeks Funds', id: 1}
+                
+    ])
 
     const [company_selected, setCompany_selected] = useState(true)
     
 
 
-    // Entity Filter
+    // Entity/UserType Filter
     function onSelect(selectedList, selectedItem) {
         if(selectedItem.entity == "Company") {
             setCompany_selected(true);
             setUsers2([...users2,...users.filter((user) => user.typeOfUser == 'Company')])
         }
             
-        
         if(selectedItem.entity == "Instructor") 
             setUsers2([...users2,...users.filter((user) => user.typeOfUser == 'Instructor')])
         
@@ -42,10 +46,10 @@ const SearchHeader = ({users, setUsers, users2, setUsers2}) => {
         
         if(selectedItem.entity == "Company"){
             setCompany_selected(false);
+            multiSelRef.companyRef.current.resetSelectedValues();
             setUsers2(users2.filter((user2) => user2.typeOfUser !== "Company"))
         }         
             
-        
         if(selectedItem.entity == "Instructor") 
             setUsers2(users2.filter((user2) => user2.typeOfUser !== "Instructor"))
 
@@ -63,7 +67,6 @@ const SearchHeader = ({users, setUsers, users2, setUsers2}) => {
         }
         
     }
-
     function onSelect_company(selectedList, selectedItem){
         if (company_selected) {
             setUsers2(users2.filter((user2) => user2.typeOfUser !== "Company" || (user2.typeOfUser === "Company" && user2.typeUser.lookingFunding === true)))
@@ -80,19 +83,34 @@ const SearchHeader = ({users, setUsers, users2, setUsers2}) => {
         return true
     }
 
+    function containsUser(userType){
+        // For when the Componet hasn't even rendered yet, by default the page starts with true when it does eventually render
+        if(!multiSelRef.userRef.current) return true;
+
+        const selectedUsers=multiSelRef.userRef.current.getSelectedItems();
+        console.log(selectedUsers);
+        for(var i = 0; i<selectedUsers.length;i++)
+            if (selectedUsers[i].entity === userType)
+                return true;
+        return false;
+    }
+
     useEffect(() => {
-        console.log("Effect Code Ran");
-        console.log("unsorted",users2);
+        console.log("Ref contains Company?",containsUser('Company'));
         
         if (!isSorted(users2)){
             const sortedusers = users2.sort((a,b) => (a.name > b.name) ? 1 : -1);
-            console.log("sorted",sortedusers);
             setUsers2([...sortedusers]);
         }
         
     });
 
-    
+    //THis by name default
+    function updateSearch(event){
+        
+        console.log("This is search",event.target.value);
+        setUsers2(users.filter((user) => user.name.toLowerCase().includes(event.target.value.toLowerCase())));
+    }
 
 
     return (
@@ -100,7 +118,7 @@ const SearchHeader = ({users, setUsers, users2, setUsers2}) => {
             <div className="profile_search_search_bar">
                 <form>
                     <label></label>
-                        <input className="form-control" type="text" name="name" placeholder="Search"/>
+                        <input className="form-control" type="text" name="name" placeholder="Search" onChange={updateSearch}/>
                 </form>
             </div>
 
@@ -109,11 +127,12 @@ const SearchHeader = ({users, setUsers, users2, setUsers2}) => {
 
             <p>Filter Entity</p>
             <Multiselect
-                options={options.options} // Options to display in the dropdown
-                selectedValues={options.options} // Preselected value to persist in dropdown
+                options={options} // Options to display in the dropdown
+                selectedValues={options} // Preselected value to persist in dropdown
                 onSelect={onSelect} // Function will trigger on select event
                 onRemove={onRemove} // Function will trigger on remove event
                 displayValue="entity" // Property name to display in the dropdown options
+                ref={multiSelRef.userRef}
                 style={{
 
                     inputField: { // To change input field position or margin
@@ -127,13 +146,17 @@ const SearchHeader = ({users, setUsers, users2, setUsers2}) => {
                     }
                   }}
                 />
+            
+            {containsUser('Company') ? 
+            <div>
             <p>Filter Company Status</p>
             <Multiselect
-                options={options_company.options} // Options to display in the dropdown
-                //selectedValues={options.options} // Preselected value to persist in dropdown
+                options={options_company} // Options to display in the dropdown
+                selectedValues={[]} // Preselected value to persist in dropdown
                 onSelect={onSelect_company} // Function will trigger on select event
                 onRemove={onRemove_company} // Function will trigger on remove event
                 displayValue="status" // Property name to display in the dropdown options
+                ref={multiSelRef.companyRef}
                 style={{
 
                     inputField: { // To change input field position or margin
@@ -146,7 +169,9 @@ const SearchHeader = ({users, setUsers, users2, setUsers2}) => {
                       color: "Black"
                     }
                   }}
-                />
+            />
+            </div>: <h3></h3>}
+            
             </div>
 
 
